983da7071320d580254b33d8eb0e720f593e89bb
lawrence_chen <lawrence91chen@gmail.com>
2024-03-25 17:47:46 +0800

    fix: IDENTITY is not default strategy for sqlite in jpa(hibernate)

--- src/main/java/com/github/lawrence91chen/entity/SysProp.java
+++ src/main/java/com/github/lawrence91chen/entity/SysProp.java
@@ -3,6 +3,7 @@ package com.github.lawrence91chen.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -16,7 +17,7 @@ import lombok.ToString;
 @Entity
 public class SysProp {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
 	@Column(name = "system")
