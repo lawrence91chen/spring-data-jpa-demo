80c40af0547453b34e186c02ca914f51f2d927ba
lawrence_chen <lawrence91chen@gmail.com>
2024-03-25 15:20:55 +0800

    refactor: avoid using lombok @Data in JPA Entity

--- src/main/java/com/github/lawrence91chen/entity/SysProp.java
+++ src/main/java/com/github/lawrence91chen/entity/SysProp.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@ToString
 @Table(name = "sys_prop")
 @Entity
-@Data
 public class SysProp {
 	@Id
 	@GeneratedValue
